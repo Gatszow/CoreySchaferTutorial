6d78e9472a326964285d6e339d56c466a57521ab
Gatszow <hrysins@gmail.com>
2020-07-09 10:36:27 +0200

    First add

---
+++ CoreyMS-Page.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://coreyms.com/').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video link'])
+
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    summary = article.find('div', class_='entry-content').p.text
+    try:
+        video_src = article.find('iframe', class_='youtube-player')['src']
+        video_id = video_src.split('/')[4]
+        video_id = video_id.split('?')[0]
+        youtube_link = f'https://youtube.com/watch?v={video_id}'
+    except Exception:
+        youtube_link = None
+    print(headline)
+    print(summary)
+    print(youtube_link)
+    print()
+
+    csv_writer.writerow([headline, summary, youtube_link])
+
+csv_file.close()
---
+++ simple.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open('simple.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+
+# soup.element returns first element with that name
+# for example soup.title will return first title element
+#match = soup.title.text
+#print(match)
+
+# soup.find('element') returns first element with that name too
+# but we can specify class_, so we can get other output
+# for example soup.find('div', class_='footer') will return last element
+# in this html file
+#match = soup.find('div', class_='footer')
+#print(match)
+
+for article in soup.find_all('div', class_='article'):
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.p.text
+    print(summary)
+
+    print()
